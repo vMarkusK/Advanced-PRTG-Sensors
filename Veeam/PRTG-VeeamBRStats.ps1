576c4a9bf2930fab8aa7d24c2dd5e3f3ce4bce5a
Markus Kraus <markus.kraus@gmail.com>
2021-02-27 21:10:50 +0100

    Fix Get-VBRBackupRepository Space Properties
    See also:
    https://forums.veeam.com/powershell-f26/v11-get-vbrbackuprepository-space-properties-t72415.html

--- Veeam/PRTG-VeeamBRStats.ps1
+++ Veeam/PRTG-VeeamBRStats.ps1
@@ -206,6 +206,73 @@ Function Get-vPCRepoInfo {
             }
 
             Write-Debug $r.Info
+            $outputObj = New-RepoObject $r.Name $Hostname $r.FriendlyPath $r.GetContainer().CachedFreeSpace.InBytes $r.GetContainer().CachedTotalSpace.InBytes
+        }
+        $outputAry += $outputObj
+    }
+    End {
+        $outputAry
+    }
+}
+
+Function Get-vPCRepoInfoPre11 {
+[CmdletBinding()]
+    param (
+        [Parameter(Position=0, ValueFromPipeline=$true)]
+        [PSObject[]]$Repository
+    )
+    Begin {
+        $outputAry = @()
+        Function New-RepoObject {param($name, $repohost, $path, $free, $total)
+        $repoObj = New-Object -TypeName PSObject -Property @{
+            Target = $name
+            RepoHost = $repohost
+                        Storepath = $path
+                        StorageFree = [Math]::Round([Decimal]$free/1GB,2)
+                        StorageTotal = [Math]::Round([Decimal]$total/1GB,2)
+                        FreePercentage = [Math]::Round(($free/$total)*100)
+            }
+        Return $repoObj | Select-Object Target, RepoHost, Storepath, StorageFree, StorageTotal, FreePercentage
+        }
+    }
+    Process {
+        Foreach ($r in $Repository) {
+            # Refresh Repository Size Info
+            try {
+                if ($PSRemote) {
+                    $SyncSpaceCode = {
+                        param($RepositoryName);
+                        [Veeam.Backup.Core.CBackupRepositoryEx]::SyncSpaceInfoToDb((Get-VBRBackupRepository -Name $RepositoryName), $true)
+                    }
+
+                    Invoke-Command -Session $RemoteSession -ScriptBlock $SyncSpaceCode -ArgumentList $r.Name
+                } else {
+                    [Veeam.Backup.Core.CBackupRepositoryEx]::SyncSpaceInfoToDb($r, $true)
+                }
+
+            }
+            catch {
+                Write-Debug "SyncSpaceInfoToDb Failed"
+                Write-Error $_.ToString()
+                Write-Error $_.ScriptStackTrace
+            }
+            If ($r.HostId -eq "00000000-0000-0000-0000-000000000000") {
+                $HostName = ""
+            }
+            Else {
+                $HostName = $(Get-VBRServer | Where-Object {$_.Id -eq $r.HostId}).Name.ToLower()
+            }
+
+            if ($PSRemote) {
+            # When veeam commands are invoked remotly they are serialized during transfer. The info property become not object but string.
+            # To gather the info following construction should be used
+                $r.info = Invoke-Command -Session $RemoteSession -HideComputerName -ScriptBlock {
+                    param($RepositoryName);
+                    (Get-VBRBackupRepository -Name $RepositoryName).info
+                } -ArgumentList $r.Name
+            }
+
+            Write-Debug $r.Info
             $outputObj = New-RepoObject $r.Name $Hostname $r.Path $r.info.CachedFreeSpace $r.Info.CachedTotalSpace
         }
         $outputAry += $outputObj
